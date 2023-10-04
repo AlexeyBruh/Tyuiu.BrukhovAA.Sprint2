4ae53f31ca670eac2fba976358524af98fa77f2c
Tyuiu.BrukhovAA@outlook.com <bruhov_uch@mail.ru>
2023-10-04 21:04:38 +0500

    Task1

---
+++ Tyuiu.BrukhovAA.Sprint2.Task1.V12/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BrukhovAA.Sprint2.Task1.V12.Lib;
+
+namespace Tyuiu.BrukhovAA.Sprint2.Task1.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Брюхов А. А. | АСОиУБ-23-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                            *");
+            Console.WriteLine("* Задание #1                                                             *");
+            Console.WriteLine("* Вариант #12                                                            *");
+            Console.WriteLine("* Выполнил: Брюхов Алексей Андреевич | АСОиУБ-23-1                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу из операций сравнения и арифметических выражений,   *");
+            Console.WriteLine("* которая вернет последовательность(массив):                             *");
+            Console.WriteLine("* (True, True, True, False, True, False)                                 *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int a = 152;
+            int b = 116;
+            int c = 887;
+            int d = 556;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
