795bea78dc8869a9a2842f13d481261fd8bc9f63
Tyuiu.BrukhovAA@outlook.com <bruhov_uch@mail.ru>
2023-10-08 14:55:30 +0500

    Task3

---
+++ Tyuiu.BrukhovAA.Sprint2.Task3.V4.Test/DataServiceTest.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.BrukhovAA.Sprint2.Task3.V4.Lib;
+
+namespace Tyuiu.BrukhovAA.Sprint2.Task3.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = Math.Round(ds.Calculate(x), 3);
+            double wait = 11.000;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = Math.Round(ds.Calculate(x), 3);
+            double wait = Math.Round(0.0833333, 3);
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -1;
+            double res = Math.Round(ds.Calculate(x), 3);
+            double wait = Math.Round(-0.50000, 3);
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -10;
+            double res = Math.Round(ds.Calculate(x), 3);
+            double wait = Math.Round(-109.9000, 3);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BrukhovAA.Sprint2.Task3.V4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BrukhovAA.Sprint2.Task3.V4.Lib;
+
+namespace Tyuiu.BrukhovAA.Sprint2.Task3.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Брюхов А. А. | АСОиУБ-23-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                            *");
+            Console.WriteLine("* Задание #3                                                             *");
+            Console.WriteLine("* Вариант #4                                                             *");
+            Console.WriteLine("* Выполнил: Брюхов Алексей Андреевич | АСОиУБ-23-1                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y     *");
+            Console.WriteLine("* с использованием вложенных оператор if-else, где пользователь вводит   *");
+            Console.WriteLine("* значение переменной X с клавиатуры.                                    *");
+            Console.WriteLine("* Округлить полученное значение до трех знаков после запятой.            *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Введите значение x");
+            double x = Convert.ToInt32(Console.ReadLine());
+            double res = Math.Round(ds.Calculate(x), 3);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+            Console.ReadKey();
+        }
+    }
+}
