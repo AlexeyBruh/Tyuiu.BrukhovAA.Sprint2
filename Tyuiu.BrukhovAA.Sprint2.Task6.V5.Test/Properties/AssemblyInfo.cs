71f19e135f0683ff6f1a5bcf6f2c6d88e821623c
Tyuiu.BrukhovAA@outlook.com <bruhov_uch@mail.ru>
2023-10-15 18:36:36 +0500

    Task6

---
+++ Tyuiu.BrukhovAA.Sprint2.Task6.V5.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BrukhovAA.Sprint2.Task6.V5.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BrukhovAA.Sprint2.Task6.V5.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a316ea36-e84b-4aee-8b4d-3ec382b47274")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.BrukhovAA.Sprint2.Task6.V5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BrukhovAA.Sprint2.Task6.V5.Lib;
+
+namespace Tyuiu.BrukhovAA.Sprint2.Task6.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Брюхов А. А. | АСОиУБ-23-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                            *");
+            Console.WriteLine("* Задание #6                                                             *");
+            Console.WriteLine("* Вариант #5                                                             *");
+            Console.WriteLine("* Выполнил: Брюхов Алексей Андреевич | АСОиУБ-23-1                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая использует сокращенную форму записи        *");
+            Console.WriteLine("*  оператора switch и вычисляет требуемое значение и возвращает результат*");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Введите порядковый номер карты");
+            int x = Convert.ToInt32(Console.ReadLine());
+            string res;
+
+            if ((x < 6) || (x > 14))
+            {
+                res = "Значение введено неверно";
+            }
+            else
+            {
+                res = "Достоинство карты: " + ds.FindCardValue(x);
+            }
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
